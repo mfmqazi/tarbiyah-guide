3f4b54018b6ab7efae67d3310e0da354f01e0ac7
Musaddique Qazi <38559064+mfmqazi@users.noreply.github.com>
2025-11-29 10:53:59 -0700

    Fix: Add basename to router and index route for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import GeneralTarbiyah from './pages/GeneralTarbiyah';
@@ -10,15 +10,17 @@ import Fatima from './pages/Fatima';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/tarbiyah-guide">
       <Layout>
         <Routes>
+          <Route index element={<Home />} />
           <Route path="/" element={<Home />} />
           <Route path="/tarbiyah" element={<GeneralTarbiyah />} />
           <Route path="/daughters" element={<RaisingDaughters />} />
           <Route path="/marriage-prep" element={<MarriagePrep />} />
           <Route path="/resources" element={<Resources />} />
           <Route path="/fatima" element={<Fatima />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </Layout>
     </Router>
